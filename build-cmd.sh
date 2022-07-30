d7c3fcc59f6f6a61d6543f58452933f34e57fc9f
Nicky Dasmijn <nicky.dasmijn@posteo.nl>
2022-07-30 20:04:34 +0200

    Call autoreconf & limit make -j to the number of cpus available

--- build-cmd.sh
+++ build-cmd.sh
@@ -6,16 +6,18 @@ GST_SOURCE="gst-plugins-base-1.8.1"
 stage=`pwd`
 
 pushd ../$SOURCE
+  autoreconf
   ./configure -prefix=$stage
-  make -j8
+  make -j`nproc`
   make install
 popd
 
 export PKG_CONFIG_PATH=${stage}/lib/pkgconfig 
 
 pushd ../$GST_SOURCE
+  autoreconf
   ./configure -prefix=$stage
-  make -j8
+  make -j`nproc` 
   make install
 popd
 
